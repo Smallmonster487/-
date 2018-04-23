a45f46bfa504c701af33052520d7ea3ffe0697b3
DESKTOP-9MF0OK7\小怪 <s11228246@gmail.com>
2018-04-23 20:36:33 +0800

    second commit

--- 大樂透/大樂透/Program.cs
+++ 大樂透/大樂透/Program.cs
@@ -27,12 +27,12 @@ namespace 大樂透
             while (time <= total)
             {
 
-                int r1 = rand.Next(0, 43);
-                int r2 = rand.Next(0, 43);
-                int r3 = rand.Next(0, 43);
-                int r4 = rand.Next(0, 43);
-                int r5 = rand.Next(0, 43);
-                int r6 = rand.Next(0, 43);
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                int r6 = rand.Next(1, 43);
 
                 //用if判斷每個數字是否有重複
                 if (r1 != r2 && r1 != r3 && r1 != r4 && r1 != r5 && r1 != r6 && r2 != r3 && r2 != r4 && r2 != r5 && r2 != r6 && r3 != r4 && r3 != r5 && r3 != r6 && r4 != r5 && r4 != r6 && r5 != r6)
